060eba4c0a99a51ccc8a3eb5361f552664cf6bb5
Wonjun Jeong <nttejun@gmail.com>
2018-10-10 22:41:55 +0900

    Added undertow servlet container and removed tomcat servlet container

--- src/main/java/starter/api/ApiApplication.java
+++ src/main/java/starter/api/ApiApplication.java
@@ -3,18 +3,22 @@ package starter.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
-import java.time.Duration;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
+@Configuration
 public class ApiApplication extends SpringBootServletInitializer {
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ApiApplication.class);
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
 }
